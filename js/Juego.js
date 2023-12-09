f60a74043090962dd7b197686276cff238a3e2cd
Sergio Ruiz <sergioruizrock@gmail.com>
2023-12-09 20:34:44 -0300

    se realizaron pequeños cambio en el main de la pagina administracion y se creo las clases juego y usuario con sus respectivos atributos y constructores

---
+++ js/Juego.js
@@ -0,0 +1,25 @@
+class Juego{
+    #codigo
+    #nombre
+    #detalles
+    #categoria
+    #precio
+    #img
+    #descripcion
+    #requisitos
+    #desarrollador
+    #resenias
+
+    constructor(codigo,nombre,detalles,categoria,precio,img,descripcion,requisitos,desarrollador,resenias){
+        this.#codigo=codigo
+        this.#nombre=nombre
+        this.#detalles=detalles
+        this.#categoria=categoria
+        this.#precio=precio
+        this.#img=img
+        this.#descripcion=descripcion
+        this.#requisitos=requisitos
+        this.#desarrollador=desarrollador
+        this.#resenias=resenias
+    }
+}
---
+++ js/Usuario.js
@@ -0,0 +1,17 @@
+class Usuario{
+    #nombre
+    #apellido
+    #tipo
+    #correo
+    #username
+    #contrasenia
+
+    constructor(nombre,apellido,tipo,correo,username,contrasenia){
+        this.#nombre=nombre
+        this.#apellido=apellido
+        this.#tipo=tipo
+        this.#correo=correo
+        this.#username=username
+        this.#contrasenia=contrasenia
+    }
+}
